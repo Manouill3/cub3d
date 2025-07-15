b2552ea75294f9c2e22ce7faa7fddcc625c0a4c3
tcybak <tcybak@student.42.fr>
2025-07-15 16:50:43 +0200

    map parsing border left before full fill

--- src/parsing/parsing_map.c
+++ src/parsing/parsing_map.c
@@ -6,7 +6,7 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:31:44 by tcybak            #+#    #+#             */
-/*   Updated: 2025/07/15 14:25:20 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/07/15 16:50:18 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,16 @@ int ft_check_border(t_cub *cub)
         hor_next = 0;
         while (cub->map[ver][hor] && cub->map[ver][hor + 1] != '\0')
             hor++;
-        if (cub->size_ver + 1 == ver)
-            return (0);
-        printf("ver act = %d ver = %d\n",cub->size_ver + 1, ver );
-        while (cub->map[ver] && cub->map[ver + 1][hor_next] && cub->map[ver + 1][hor_next + 1] != '\0')
+        if (!cub->map[ver + 1])
+            break;
+        while (cub->map[ver] && cub->map[ver + 1] && cub->map[ver + 1][hor_next] && cub->map[ver + 1][hor_next + 1] != '\0')
             hor_next++;
-        if (cub->map[ver][hor] != '1' || cub->map[ver + 1][hor_next] != '1')
+        if (cub->map[ver][hor] != '1' || cub->map[ver + 1][hor_next] != '1' )
         {
-            printf("ver %d hor %d num = %c num_next = %c\n", ver, hor, cub->map[ver][hor], cub->map[ver + 1][hor_next]);
             write(2, "bad map edge\n", 13);    
             return (1);
         }
         ver++;
     }
-    printf("finish\n");
     return (0);
 }
